919f6fcf5dabff14b09521516adc1d1ee4e615bc
khw1031 <khw1031@gmail.com>
2026-05-18 15:03:35 +0900

    glowed v0.2.1

--- Formula/glowed.rb
+++ Formula/glowed.rb
@@ -1,8 +1,8 @@
 class Glowed < Formula
   desc "Ghostty-oriented terminal TUI Markdown browser/editor"
   homepage "https://github.com/khw1031/glowed"
-  url "https://github.com/khw1031/glowed/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "19d1239ac12d9dbc3c6e66e6843e634dfb1d0882f608647a705779dc957b8039"
+  url "https://github.com/khw1031/glowed/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "f2efe96fbf3082270f77723bf0e27729bc98599f5eafb8b2ca8f80d61721ced2"
   license "MIT"
 
   depends_on "go" => :build
